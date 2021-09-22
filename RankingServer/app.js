a43ae808c8b1a92f8a0368e7255a987a6a0b600a
idea7654 <idea7654@naver.com>
2021-09-22 22:00:13 +0900

    add ranking client dll

--- RankingServer/app.js
+++ RankingServer/app.js
@@ -3,6 +3,13 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
 
+const https = require("https");
+const fs = require("fs");
+const options = {
+  key: fs.readFileSync("./privkey.pem"),
+  cert: fs.readFileSync("./cert.pem"),
+};
+
 let rankings = [{ gameName: "test", data: [] }];
 
 app.use(
@@ -13,31 +20,37 @@ app.use(
 );
 app.use(bodyParser.json());
 
+app.get("/", (req, res) => {
+  console.log("응답있음");
+  res.send("test");
+});
+
 app.post("/", (req, res) => {
-  if (req.body.gameName && req.body.nickName && req.body.score) {
+  console.log(req.body);
+  if (req.body.gameName && req.body.userName && req.body.score) {
     //res.send("하위");
     const index = rankings.findIndex((i) => i.gameName == req.body.gameName);
     if (index != -1) {
       const userIndex = rankings[index].data.findIndex(
-        (i) => i.nickName == req.body.nickName
+        (i) => i.userName == req.body.userName
       );
       if (userIndex == -1) {
         rankings[index].data.push({
-          nickName: req.body.nickName,
+          userName: req.body.userName,
           score: req.body.score,
         });
       } else {
         rankings[index].data[userIndex].score = req.body.score;
       }
       //res.status(200).json(rankings[index]);
-      res.send(rankings[index]);
+      res.json(rankings[index]);
     } else {
       res.send("Invalid Game");
     }
   } else {
     res.send("Invalid Data Type");
   }
-  console.log(rankings[0].data);
 });
 
-app.listen(8000);
+https.createServer(options, app).listen(8600);
+// app.listen(8600);
